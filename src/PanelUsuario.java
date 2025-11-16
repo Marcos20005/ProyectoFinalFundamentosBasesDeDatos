b65e3b2d188d596d643be65318532f4e04fe0591
marcos-ortega <mo205016@gmail.com>
2025-11-15 20:53:40 -0700

    Verificaciones final

--- src/PanelUsuario.java
+++ src/PanelUsuario.java
@@ -2,12 +2,10 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -15,137 +13,138 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
-import java.sql.CallableStatement;
 
-public class PanelUsuario extends JPanel{
+public class PanelUsuario extends JPanel {
+
     //Objetos de conexion SQL
     CallableStatement stmt = null;
     Connection con = null;
-    
 
     public PanelUsuario(MantenimientoUsuario controlOriginal, int funcion) throws ClassNotFoundException, SQLException {
         MantenimientoUsuario control = controlOriginal;
         //Estableciendo conexion a la base de datos
         Class.forName("com.mysql.cj.jdbc.Driver");
         con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cine?verifyServerCertificate=false&useSSL=true", "root", "cRojas34");
-        stmt = (CallableStatement) con.prepareCall("{CALL insertarDatosEstudiantes(?, ?, ?, ?, ?, ?, ?)}");
-        
-        if (funcion==0) {
-           JLabel lblTitulo = crearEtiqueta("Datos de nuevo registro", 250, 20, 300, 30);
-this.add(lblTitulo); 
-        }else{
-             JLabel lblTitulo = crearEtiqueta("Actualizar registro", 250, 20, 300, 30);
-this.add(lblTitulo);
-        }
+        stmt = (CallableStatement) con.prepareCall("{CALL insertar_usuario(?, ?, ?, ?, ?, ?, ?)}");
 
+        if (funcion == 0) {
+            JLabel lblTitulo = crearEtiqueta("Datos de nuevo registro", 250, 20, 300, 30);
+            this.add(lblTitulo);
+        } else {
+            JLabel lblTitulo = crearEtiqueta("Actualizar registro", 250, 20, 300, 30);
+            this.add(lblTitulo);
+        }
 
 //Campos de texto
-JLabel lblID = crearEtiqueta("ID:", 150, 70, 140, 30);
-JTextField txtID = crearCampoTexto(300, 70, 200, 30, "Ingrese ID del usuario");
-this.add(lblID);
-this.add(txtID);
+        JLabel lblID = crearEtiqueta("ID:", 150, 70, 140, 30);
+        JTextField txtID = crearCampoTexto(300, 70, 200, 30, "Ingrese ID del usuario");
+        this.add(lblID);
+        this.add(txtID);
 
-JLabel lblNombre1 = crearEtiqueta("Primer Nombre:", 150, 110, 140, 30);
-JTextField txtNombre1 = crearCampoTexto(300, 110, 200, 30, "Ingrese primer nombre");
-this.add(lblNombre1);
-this.add(txtNombre1);
+        JLabel lblNombre1 = crearEtiqueta("Primer Nombre:", 150, 110, 140, 30);
+        JTextField txtNombre1 = crearCampoTexto(300, 110, 200, 30, "Ingrese primer nombre");
+        this.add(lblNombre1);
+        this.add(txtNombre1);
 
-JLabel lblNombre2 = crearEtiqueta("Segundo Nombre:", 150, 150, 140, 30);
-JTextField txtNombre2 = crearCampoTexto(300, 150, 200, 30, "Ingrese segundo nombre");
-this.add(lblNombre2);
-this.add(txtNombre2);
+        JLabel lblNombre2 = crearEtiqueta("Segundo Nombre:", 150, 150, 140, 30);
+        JTextField txtNombre2 = crearCampoTexto(300, 150, 200, 30, "Ingrese segundo nombre");
+        this.add(lblNombre2);
+        this.add(txtNombre2);
 
-JLabel lblApellido1 = crearEtiqueta("Primer Apellido:", 150, 190, 140, 30);
-JTextField txtApellido1 = crearCampoTexto(300, 190, 200, 30, "Ingrese primer apellido");
-this.add(lblApellido1);
-this.add(txtApellido1);
+        JLabel lblApellido1 = crearEtiqueta("Primer Apellido:", 150, 190, 140, 30);
+        JTextField txtApellido1 = crearCampoTexto(300, 190, 200, 30, "Ingrese primer apellido");
+        this.add(lblApellido1);
+        this.add(txtApellido1);
 
-JLabel lblApellido2 = crearEtiqueta("Segundo Apellido:", 150, 230, 140, 30);
-JTextField txtApellido2 = crearCampoTexto(300, 230, 200, 30, "Ingrese segundo apellido");
-this.add(lblApellido2);
-this.add(txtApellido2);
+        JLabel lblApellido2 = crearEtiqueta("Segundo Apellido:", 150, 230, 140, 30);
+        JTextField txtApellido2 = crearCampoTexto(300, 230, 200, 30, "Ingrese segundo apellido");
+        this.add(lblApellido2);
+        this.add(txtApellido2);
 
-JLabel lblUsuario = crearEtiqueta("Usuario:", 150, 270, 140, 30);
-JTextField txtUsuario = crearCampoTexto(300, 270, 200, 30, "Ingrese nombre de usuario");
-this.add(lblUsuario);
-this.add(txtUsuario);
+        JLabel lblUsuario = crearEtiqueta("Usuario:", 150, 270, 140, 30);
+        JTextField txtUsuario = crearCampoTexto(300, 270, 200, 30, "Ingrese nombre de usuario");
+        this.add(lblUsuario);
+        this.add(txtUsuario);
 
-JLabel lblClave = crearEtiqueta("Contraseña:", 150, 310, 140, 30);
-JTextField txtClave = crearCampoTexto(300, 310, 200, 30, "Ingrese contraseña");
-this.add(lblClave);
-this.add(txtClave);
+        JLabel lblClave = crearEtiqueta("Contraseña:", 150, 310, 140, 30);
+        JTextField txtClave = crearCampoTexto(300, 310, 200, 30, "Ingrese contraseña");
+        this.add(lblClave);
+        this.add(txtClave);
 
+        JButton botonGuardar = crearBoton("Guardar", 270, 360, 100, 40, "Guardar nuevo usuario", "Iconos/guardar-el-archivo.png");
+        botonGuardar.setBackground(new Color(46, 204, 113));
+        botonGuardar.setForeground(Color.WHITE);
+        this.add(botonGuardar);
 
-JButton botonGuardar = crearBoton("Guardar", 270, 360, 100, 40, "Guardar nuevo usuario", "Iconos/guardar-el-archivo.png");
-botonGuardar.setBackground(new Color(46, 204, 113));
-botonGuardar.setForeground(Color.WHITE);
-this.add(botonGuardar);
+        JButton botonCancelar = crearBoton("Cancelar", 400, 360, 100, 40, "Regresar atras", "Iconos/cancelar.png");
+        botonCancelar.setBackground(new Color(240, 128, 128));
+        botonCancelar.setForeground(Color.WHITE);
+        this.add(botonCancelar);
+        botonCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txtID.setText("");
+                txtNombre1.setText("");
+                txtNombre2.setText("");
+                txtApellido1.setText("");
+                txtApellido2.setText("");
+                txtUsuario.setText("");
+                txtClave.setText("");
+                control.add(control.botonActualizar);
+                control.add(control.botonInsertar);
+                control.add(control.botonEliminar);
+                control.add(control.botonConsultar);
+                control.add(control.scroll);
+                control.remove(control.panel);
+                control.recargarTabla();
+                control.revalidate();
+                control.repaint();
+            }
+        });
 
-JButton botonCancelar = crearBoton("Cancelar", 400, 360, 100, 40, "Regresar atras", "Iconos/cancelar.png");
-botonCancelar.setBackground(new Color(240, 128, 128));
-botonCancelar.setForeground(Color.WHITE);
-this.add(botonCancelar);
-botonCancelar.addActionListener(new ActionListener() {
-    @Override
-    public void actionPerformed(ActionEvent e){
- txtID.setText("");
-     txtNombre1.setText("");
-     txtNombre2.setText("");
-     txtApellido1.setText("");
-     txtApellido2.setText("");
-     txtUsuario.setText("");
-     txtClave.setText("");
-     control.add(control.botonActualizar);
-     control.add(control.botonInsertar);
-     control.add(control.botonEliminar);
-     control.add(control.botonConsultar);
-     control.add(control.scroll);
-     control.remove(control.panel);
-     control.recargarTabla();
-     control.revalidate();
-     control.repaint();
+        botonGuardar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    int eleccion = JOptionPane.showConfirmDialog(null, "¿Desea confirmar nuevo registro?", "Confirmar acción", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                    if (eleccion == 0) {
+                        // stmt = con.prepareCall("{Call insertar_usuario(?,?,?,?,?,?,?)}");
+                        stmt.setString(1, txtID.getText());
+                        stmt.setString(2, txtNombre1.getText());
+                        stmt.setString(3, txtNombre2.getText());
+                        stmt.setString(4, txtApellido1.getText());
+                        stmt.setString(5, txtApellido2.getText());
+                        stmt.setString(6, txtUsuario.getText());
+                        stmt.setString(7, txtClave.getText());
+                        stmt.executeUpdate();
+                    }
+
+                } catch (SQLException e1) {
+                    JOptionPane.showMessageDialog(null, e1.getMessage(), "Advertencia", JOptionPane.WARNING_MESSAGE);
+                }
+                txtID.setText("");
+                txtNombre1.setText("");
+                txtNombre2.setText("");
+                txtApellido1.setText("");
+                txtApellido2.setText("");
+                txtUsuario.setText("");
+                txtClave.setText("");
+                control.add(control.botonActualizar);
+                control.add(control.botonInsertar);
+                control.add(control.botonEliminar);
+                control.add(control.botonConsultar);
+                control.add(control.scroll);
+                control.remove(control.panel);
+                control.recargarTabla();
+                control.revalidate();
+                control.repaint();
+
+            }
+
+        });
+
     }
-});
 
-
-botonGuardar.addActionListener(new ActionListener() {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-     try {
-        int eleccion = JOptionPane.showConfirmDialog(null,"¿Desea confirmar nuevo registro?","Confirmar acción",JOptionPane.YES_NO_OPTION,    JOptionPane.QUESTION_MESSAGE);
-        if (eleccion==0) {
-           stmt.executeUpdate("INSERT INTO usuario (cedula, nombre1, nombre2, apellido1, apellido2, login, clave) VALUES ('"+txtID.getText()+"', '"+txtNombre1.getText()+"', '"+txtNombre2.getText()+"', '"+txtApellido1.getText()+"', '"+txtApellido1.getText()+"', '"+txtUsuario.getText()+"', '"+txtClave.getText()+"');"); 
-        }
-        
-     } catch (SQLException e1) {
-        JOptionPane.showMessageDialog(null, "Hubo un error por favor vuelva a intentar","Advertencia",JOptionPane.WARNING_MESSAGE);
-     }
-     txtID.setText("");
-     txtNombre1.setText("");
-     txtNombre2.setText("");
-     txtApellido1.setText("");
-     txtApellido2.setText("");
-     txtUsuario.setText("");
-     txtClave.setText("");
-     control.add(control.botonActualizar);
-     control.add(control.botonInsertar);
-     control.add(control.botonEliminar);
-     control.add(control.botonConsultar);
-     control.add(control.scroll);
-     control.remove(control.panel);
-     control.recargarTabla();
-     control.revalidate();
-     control.repaint();
-     
-     
-     
-    }
-
-   
-});
-
-    }
-    
     static public JButton crearBoton(String texto, int x, int y, int ancho, int alto, String toolTip, String ruta) {
         JButton boton = new JButton(texto);
         boton.setBounds(x, y, ancho, alto);
@@ -154,18 +153,19 @@ botonGuardar.addActionListener(new ActionListener() {
         boton.setIcon(iconoOriginal);
         boton.setHorizontalAlignment(SwingConstants.LEFT);
         if (texto.equals("")) {
-           boton.setHorizontalAlignment(SwingConstants.CENTER);
+            boton.setHorizontalAlignment(SwingConstants.CENTER);
         }
         return boton;
     }
 
-      //Metodo declarado para crear campos de texto
+    //Metodo declarado para crear campos de texto
     static public JTextField crearCampoTexto(int x, int y, int ancho, int alto, String toolTip) {
         JTextField campoTexto = new JTextField();
         campoTexto.setBounds(x, y, ancho, alto);
         campoTexto.setToolTipText(toolTip);
         return campoTexto;
     }
+
     //Metodo declarado para crear etiquetas
     static public JLabel crearEtiqueta(String texto, int x, int y, int ancho, int alto) {
         JLabel etiqueta = new JLabel(texto);
